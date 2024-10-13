9a17fbe8ace3092d1c5503333b78e77c8d73d2ad
DaMightyDodo <73170977+DaMightyDodo@users.noreply.github.com>
2024-10-13 23:44:47 +0700

    Task 2 fixed and completed
    what is a clantag? Like clash of clans' clans?

--- task-2/main.cpp
+++ task-2/main.cpp
@@ -6,41 +6,30 @@ void main()
     int age;
     string clantag;
     string username;
-    cout << "what's your name, username, clantag, age: \n";
-
-    cin >> name >> username >> clantag >> age;
-
-
-    cout << "Hello " << name << "!\n";
-    cout << "Name: " << name;
+    cout << "Enter your name: ";
+    cin >> name;
+    cout << "\nEnter your clantag: ";
+    cin >> clantag;
+    cout << "\nEnter your age: ";
+    cin >> age;
+    cout << "\nEnter your username: ";
+    cin >> username;
+    cout << "\nHello " << name << "!";
+    cout << "\nName: " << name;
     cout << "\nUsername: [" << clantag << "]" << username;
     cout << "\nAge: " << age;
-
-
-
-
-
-
-
-
-
-
-
-
-
-    //Add code to prompt the player for their name, and then
-    //greet them with that name. For example, 
-    //
-    // Please enter your name: ben
-    // Hello ben!
-    //
-
-    //Expand this so the player is asked for their name, clan tag,
-    //age and username. Print out these details like so:
-    //
-    // - Name: <name>
-    // - Username: [<clantag>]<username>
-    // - Age: <age>
-    //
-    
 }
+//Add code to prompt the player for their name, and then
+  //greet them with that name. For example, 
+  //
+  // Please enter your name: ben
+  // Hello ben!
+  //
+
+  //Expand this so the player is asked for their name, clan tag,
+  //age and username. Print out these details like so:
+  //
+  // - Name: <name>
+  // - Username: [<clantag>]<username>
+  // - Age: <age>
+  //
